b8888aacb7008f4e57a05ebbac9bafc7f9e2b90b
sc <chang.su@autocore.ai>
2021-05-20 14:17:30 +0800

    add onnx export

--- debug_inference.py
+++ debug_inference.py
@@ -12,6 +12,9 @@ import cv2
 
 
 if __name__ == "__main__":
+    """
+    python debug_inference.py configs/autocore.py
+    """
     args, cfg = merge_config()
 
     print(autocore_row_anchor)
@@ -47,6 +50,20 @@ if __name__ == "__main__":
     input=input.cuda()
     out = net(input)
     # print(out.shape) 
+
+    # 保存为onnx格式
+    onnx_model_name = test_model[-9:-4] + '.onnx'
+    torch.onnx.export(net,               # model being run
+                  input,                         # model input (or a tuple for multiple inputs)
+                  onnx_model_name,   # where to save the model (can be a file or file-like object)
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=10,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['input'],   # the model's input names
+                  output_names = ['output'], # the model's output names
+                #   dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                #                 'output' : {0 : 'batch_size'}}
+                                )
 
     # 后处理
     out_j = out[0].data.cpu().numpy() #[grid+1,anchor_lane_number,max_lane_num]
